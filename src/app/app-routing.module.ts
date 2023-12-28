f0d8fdb36a656c91689d44becb1b19502d737b2d
Randy Birdsey <birdseyrk@gmail.com>
2023-12-28 11:09:57 -0700

    change routing to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersComponent } from './servers/servers.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'status', component: HomeComponent },
   { path: 'servers', component:ServersComponent},
   { path: 'server',  component:ServerComponent},
   { path: 'server/:server',  component:ServerComponent},
