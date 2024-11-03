966c675a51b64eea9a2cd19e1d9fc1f5ebca3495
birdseyrk <birdseyrk@gmail.com>
2024-11-03 11:15:59 -0700

    Initial Delivery

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ ansible-playbook -i /local/serveruistatus/ansible/inventory.txt /local/serveruis
 sleep 10;
 ansible-playbook -i /local/serveruistatus/ansible/inventory.txt /local/serveruistatus/ansible/remove-cleanup-local-image.yaml;
 sleep 5;
-ansible-playbook -i /local/serveruistatus/ansible/inventory.txt /local/serveruistatus/ansible/buildandpushimage.yaml;
+ansible-playbook -i /local/serveruistatus/ansible/inventory.txt /local/serveruistatus/ansible/buildandpushstatusui.yaml;
 sleep 5;
 /usr/bin/ansible-playbook -i /local/serveruistatus/ansible/inventory.txt /local/serveruistatus/ansible/apply-k8s-apps.yaml --extra-vars "myhost=k8s myfile=/local/serveruistatus/kubernetes/ui-deployment.yaml"
